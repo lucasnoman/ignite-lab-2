3ca0360f6ec8c26ba3868aaf9e2a84d7746edc8e
Lucas C. Noman <lucas.noman@outlook.com>
2023-03-31 16:28:54 -0300

    feat: changes color of cards current selected for lessons

--- src/components/Lesson.tsx
+++ src/components/Lesson.tsx
@@ -1,7 +1,8 @@
 import { format, isPast } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
 import { CheckCircle, Lock } from 'phosphor-react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
+import classNames from 'classnames';
 
 interface LessonProps {
   title: string;
@@ -11,23 +12,38 @@ interface LessonProps {
 }
 
 export function Lesson({ availableAt, slug, title, type }: LessonProps) {
+  const { slug: slugFromUrl } = useParams<{ slug: string }>();
+
   const isLessonAvailable = isPast(availableAt);
   const availableDateFormatted = format(
     availableAt,
     "EEEE' • 'd' de 'MMMM' • 'k'h'mm",
-    {
-      locale: ptBR,
-    }
+    { locale: ptBR }
   );
+
+  const isActiveLesson = slugFromUrl === slug;
 
   return (
     <Link to={`/event/lesson/${slug}`} className='group'>
       <span className='text-gray-300'>{availableDateFormatted}</span>
 
-      <div className='rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500'>
+      <div
+        className={classNames(
+          'rounded border border-gray-500 p-4 mt-2 group-hover:border-green-500',
+          { 'bg-green-500': isActiveLesson }
+        )}
+      >
         <header className='flex items-center justify-between'>
           {isLessonAvailable ? (
-            <span className='text-sm text-blue-500 font-medium flex items-center gap-2'>
+            <span
+              className={classNames(
+                'text-sm font-medium flex items-center gap-2',
+                {
+                  'text-white': isActiveLesson,
+                  'text-blue-500': !isActiveLesson,
+                }
+              )}
+            >
               <CheckCircle size={20} />
               Conteúdo liberado
             </span>
@@ -38,12 +54,27 @@ export function Lesson({ availableAt, slug, title, type }: LessonProps) {
             </span>
           )}
 
-          <span className='text-xs rounded px-2 py-[0.125rem] text-white border border-green-300 font-bold'>
+          <span
+            className={classNames(
+              'text-xs rounded px-2 py-[0.125rem] text-white border font-bold',
+              {
+                'border-white': isActiveLesson,
+                'border-green-300': !isActiveLesson,
+              }
+            )}
+          >
             {type === 'live' ? 'AO VIVO' : 'AULA PRÁTICA'}
           </span>
         </header>
 
-        <strong className='text-gray-200 mt-5 block'>{title}</strong>
+        <strong
+          className={classNames('mt-5 block', {
+            'text-white': isActiveLesson,
+            'text-gray-200': !isActiveLesson,
+          })}
+        >
+          {title}
+        </strong>
       </div>
     </Link>
   );
