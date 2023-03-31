f88eae47a56f729561f40fb988c6afd88551e621
Lucas C. Noman <lucas.noman@outlook.com>
2023-03-31 15:54:26 -0300

    feat: creates user and redirects to lesson page

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -2,6 +2,7 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 export const client = new ApolloClient({
-  uri: 'https://api-sa-east-1.hygraph.com/v2/clfv8il8q4es301unaeag9k3z/master',
+  uri: import.meta.env.VITE_API_URL,
+  headers: { Authorization: `Bearer ${import.meta.env.VITE_API_ACCESS_TOKEN}` },
   cache: new InMemoryCache(),
 });
